a4e2370d647ff64ffe350492b1fd971021dbeff2
victor <victor.leine@skola.taby.se>
2023-04-27 13:52:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let cart_dropdown = document.getElementById("cart_dropdown")
+
+function cart_icon() {
+    if (cart_dropdown.style.display === "none") {
+      cart_dropdown.style.display = "block";
+    } else {
+      cart_dropdown.style.display = "none";
+    }
+  }
+
+
+let cart_array = []
