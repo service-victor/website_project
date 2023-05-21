f0ecd668fe1afbd296006d9897ffdb33edb5a05a
victor <victor.leine@skola.taby.se>
2023-05-21 15:46:22 +0200

    completed website

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let cart = []
+
 
 let cart_dropdown = document.getElementById("cart_dropdown")
 
@@ -7,7 +9,36 @@ function cart_icon() {
     } else {
       cart_dropdown.style.display = "none";
     }
+    if (cart.length === 0) {
+      document.getElementById("empty_msg").innerHTML = "Din varukorg är tom."
+    }
   }
 
 
-let cart_array = []
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
